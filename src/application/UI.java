cfb491d73d7da4d75033b6d7f64abd361bfcf579
Bruno Basques <bruno_basques@outlook.com>
2023-10-17 09:24:39 -0300

    Placing pieces on the board

--- src/application/UI.java
+++ src/application/UI.java
@@ -9,19 +9,27 @@ public class UI {
 			System.out.print((8 - i + " "));
 			
 			for(int j = 0; j<pieces.length; j++) {
-					printPiece(pieces[i][j]);
+					printPiece(pieces, i, j);
 				}
 			System.out.println();
 			}
 		System.out.println("  a b c d e f g h");
 	}
 	
-	private static void printPiece(ChessPiece piece) {
-		if(piece == null) {
-			System.out.print("~");
+	private static void printPiece(ChessPiece[][] pieces, int row, int column) {
+		if(pieces[row][column] != null) {
+			System.out.print(pieces[row][column]);			
 		}
 		else {
-			System.out.print(piece);
+			if(row % 2 == 0 && column % 2 == 0 ||
+					row % 2 != 0 && column % 2 != 0)
+			{
+				System.out.print("~");
+			}
+			else
+			{
+				System.out.print("-");
+			}			
 		}
 		
 		System.out.print(" ");
